0cab5ea39342d8e1a1cf0c9ec31670069730610a
tgiannotta <t.giannotta92@gmail.com>
2021-11-12 17:15:00 +0100

    Aggiunti vari alert e tolti i commenti

--- js/script.js
+++ js/script.js
@@ -1,42 +1,28 @@
-/*Il programma dovrà chiedere all'utente:
-    - il numero di chilometri che vuole percorrere;
-    - Età del passeggero;
-
-    Calcolare il prezzo totale del viaggio, secondo queste regole:
-        - Il prezzo del biglietto è definito in base ai km (0.21 € al km);
-        - Va applicato uno sconto del 20% per i minorenni;
-        - Va applicato uno sconto del 40% per gli over 65;
-
-L'output del prezzo finale va messo fuori(con massimo due decimali) */
-
 //Kilometri
 const numKm = parseInt( prompt('Inserisci il numero di Kilometri da percorrere: ') );
-console.log(numKm);
 //Anni
 const etaUtente = parseInt( prompt('Quanti anni hai?') );
-console.log(etaUtente);
+
 //prezzo biglietto
 let prezzoTicket = numKm * 0.21;
-console.log(prezzoTicket);
 //Sconto in base all'età
 let sconto;
 let totale;
 if (etaUtente > 65) {
     sconto = (prezzoTicket * 40) / 100;
     totale = prezzoTicket - sconto;
-    console.log(totale);
+    alert('Avendo un età superiore a 65 anni, hai diritto ad uno sconto del 40%')
 }else if (etaUtente < 18) {
     sconto = (prezzoTicket * 20) / 100;
     totale = prezzoTicket - sconto;
-    console.log(totale);
-
+    alert('Avendo un età inferiore a 18 anni, hai diritto ad uno sconto del 20%')
 }else {
     totale = prezzoTicket
+    alert('Purtroppo, non hai diritto ad uno sconto.')
 }
 //OUTPUT
-alert(totale);
+alert('Quindi il totale da pagare è: ' + totale.toFixed(2) + 'euro');
 
 
 
 
-//let prezzoTicket = numKm * (0.21 - ((0.21 * sconto ) / 100));
